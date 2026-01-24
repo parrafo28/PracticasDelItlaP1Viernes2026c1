7c7c578cd4848d610be518578d3e7a861cb8a385
Starling Germosen <sgermosen@outlook.com>
2026-01-23 20:56:49 -0400

    Subiendo la primera clase

---
+++ MyLearning/Program1/MyStaticClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program1
+{
+    public static class MyStaticClass
+    {
+        public static int MyStaticMethod()
+        {
+            return 42;
+        }
+    }
+}
